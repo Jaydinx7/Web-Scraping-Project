f6283d5df75f037c4e1334196746854da99f8314
Jaydin Solomons <jasolcpt025@student.wethinkcode.co.za>
2026-06-17 16:59:06 +0200

    Feat: Added Welcome Text and input for search in github. Closes #2

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from re import search
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
+print("|======================|")
+print("| WELCOME TO GITSCRAPE |")
+print("|======================|\n\n")
 
-driver.get("https://github.com/search?q=python&type=repositories")
+search = input("What are you searching on GitHub?: ").lower().strip()
+
+driver.get("https://github.com/search?q="+search+"&type=repositories")
 
 time.sleep(5)
 
