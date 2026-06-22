026c5a2eb0126ae4b84f2d45fe91a9ca0e885f0b
Jaydin Solomons <jasolcpt025@student.wethinkcode.co.za>
2026-06-22 10:49:15 +0200

    Feat: Appends a dictionary of repo and url to a list of Scraped Data and added try-except for error handling. Closes #3

--- main.py
+++ main.py
@@ -4,15 +4,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
 print("|======================|")
 print("| WELCOME TO GITSCRAPE |")
 print("|======================|\n\n")
 
 search = input("What are you searching on GitHub?: ").lower().strip()
 
+driver = webdriver.Chrome()
+
 driver.get("https://github.com/search?q="+search+"&type=repositories")
 
 time.sleep(5)
 
+repo_elements = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='results-list'] div.search-title a")
+
+scraped_data = []
+
+for repo in repo_elements:
+    try:
+        # Get the Repo name
+        repo_name = repo.text
+
+        # Gets Repo Url
+        repo_url = repo.get_attribute('href')
+
+        if repo_name:
+            scraped_data.append({
+                "Repository": repo_name,
+                "URL": repo_url
+            })
+
+    except Exception as e:
+        continue
+
+
+print(scraped_data)
 driver.quit()
